4a9d0c68864439179094ef6c0bc06ee54e87d807
Cunibon <52969914+Cunibon@users.noreply.github.com>
2025-07-05 21:04:29 +0200

    open share dialog instead of saving locally

--- lib/widgets/settings_screen/settings_screen.dart
+++ lib/widgets/settings_screen/settings_screen.dart
@@ -3,32 +3,38 @@ import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:recipe_list/application_constants.dart';
+import 'package:recipe_list/providers/application_path_provider.dart';
 import 'package:recipe_list/widgets/generic/navigation_drawer_scaffold.dart';
+import 'package:share_plus/share_plus.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return NavigationDrawerScaffold(
       body: Column(
         children: [
           TextButton.icon(
             style: TextButton.styleFrom(alignment: Alignment.centerLeft),
             onPressed: () async {
-              final dir = await FilePicker.platform.getDirectoryPath();
-              if (dir == null) {
-                throw Exception("Could not find the downloads directory");
-              }
+              final dir = await ref.read(applicationPathProvider.future);
 
               final allData = {
                 for (final key in dataKeys) key: localStorage.getItem(key),
               };
 
-              final file = File("$dir/recipe_list.json");
+              final filePath = "${dir.path}/recipe_list.json";
+
+              final file = File(filePath);
               await file.writeAsString(jsonEncode(allData));
+
+              final params = ShareParams(files: [XFile(filePath)]);
+
+              await SharePlus.instance.share(params);
             },
             icon: Icon(Icons.upload),
             label: Text("Export data"),
