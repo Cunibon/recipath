0acc79a8f1a4cd30ee2b8dbd109cd067e5a6d635
Cunibon <52969914+Cunibon@users.noreply.github.com>
2025-07-05 13:09:40 +0200

    Added file export/import

--- lib/application_constants.dart
+++ lib/application_constants.dart
@@ -1,3 +1,10 @@
+const List<String> dataKeys = [
+  recipeDataKey,
+  groceryDataKey,
+  shoppingDataKey,
+  storageDataKey,
+];
+
 const String recipeDataKey = "recipeData";
 const String groceryDataKey = "groceryData";
 const String shoppingDataKey = "shoppingData";
--- lib/widgets/main_screen/main_screen.dart
+++ lib/widgets/main_screen/main_screen.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:recipe_list/application_constants.dart';
 import 'package:recipe_list/data/recipe_data.dart';
 import 'package:recipe_list/root_routes/root_routes.dart';
 import 'package:recipe_list/widgets/generic/navigation_drawer_scaffold.dart';
@@ -19,6 +25,45 @@ class MainScreen extends ConsumerWidget {
     final groceries = ref.watch(groceryNotifierProvider);
 
     return NavigationDrawerScaffold(
+      actions: [
+        IconButton(
+          onPressed: () async {
+            final dir = await FilePicker.platform.getDirectoryPath();
+            if (dir == null) {
+              throw Exception("Could not find the downloads directory");
+            }
+
+            final allData = {
+              for (final key in dataKeys) key: localStorage.getItem(key),
+            };
+
+            final file = File("$dir/recipe_list.json");
+            await file.writeAsString(jsonEncode(allData));
+          },
+          icon: Icon(Icons.download),
+        ),
+        IconButton(
+          onPressed: () async {
+            FilePickerResult? result = await FilePicker.platform.pickFiles(
+              type: FileType.custom,
+              allowedExtensions: ["json"],
+            );
+
+            if (result != null) {
+              File file = File(result.files.single.path!);
+
+              final data = jsonDecode(await file.readAsString());
+
+              if (data is Map) {
+                for (final key in dataKeys) {
+                  localStorage.setItem(key, jsonEncode(data[key]));
+                }
+              }
+            }
+          },
+          icon: Icon(Icons.upload),
+        ),
+      ],
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.go(
           "${RootRoutes.mainRoute.path}/${MainRoutes.createRecipe.path}",
