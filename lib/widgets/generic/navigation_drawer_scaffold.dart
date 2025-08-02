43417dbd89707141467618e49f6c5f12119b4837
Cunibon <52969914+Cunibon@users.noreply.github.com>
2025-08-02 15:14:44 +0200

    path changes

--- lib/widgets/generic/navigation_drawer_scaffold.dart
+++ lib/widgets/generic/navigation_drawer_scaffold.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:recipe_list/widgets/providers/drawer_destination_notifier.dart';
-import 'package:recipe_list/widgets/recipe_screen/drawer_destination.dart';
+import 'package:recipe_list/widgets/screens/providers/drawer_destination_notifier.dart';
+import 'package:recipe_list/widgets/screens/recipe_screen/drawer_destination.dart';
 
 class NavigationDrawerScaffold extends ConsumerWidget {
   const NavigationDrawerScaffold({
