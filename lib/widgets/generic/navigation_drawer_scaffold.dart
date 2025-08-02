7a2ebbd24623e9f6339ff080fd0e7fe7e70bc71d
Cunibon <52969914+Cunibon@users.noreply.github.com>
2025-08-02 15:16:59 +0200

    moved provider up

--- lib/widgets/generic/navigation_drawer_scaffold.dart
+++ lib/widgets/generic/navigation_drawer_scaffold.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:recipe_list/widgets/screens/providers/drawer_destination_notifier.dart';
+import 'package:recipe_list/widgets/providers/drawer_destination_notifier.dart';
 import 'package:recipe_list/widgets/screens/recipe_screen/drawer_destination.dart';
 
 class NavigationDrawerScaffold extends ConsumerWidget {
