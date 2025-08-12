02f7090fca76367813edf1bec7cc4f72e4ee7fa2
Cunibon <52969914+Cunibon@users.noreply.github.com>
2025-08-12 16:53:24 +0200

    moved auth buttons and added email

--- lib/widgets/generic/navigation_drawer_scaffold.dart
+++ lib/widgets/generic/navigation_drawer_scaffold.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:recipe_list/domain_service/syncing_service/syncing_service/syncing_service_notifier.dart';
-import 'package:recipe_list/drift/database_notifier.dart';
-import 'package:recipe_list/widgets/generic/auth_dialog.dart';
+import 'package:recipe_list/widgets/authentication/auth_buttons.dart';
 import 'package:recipe_list/widgets/providers/drawer_destination_notifier.dart';
-import 'package:recipe_list/widgets/providers/supabase/supabase_client_notifier.dart';
-import 'package:recipe_list/widgets/providers/supabase/supabase_user_notifier.dart';
 import 'package:recipe_list/widgets/screens/recipe_screen/drawer_destination.dart';
 
 class NavigationDrawerScaffold extends ConsumerWidget {
@@ -83,33 +80,7 @@ class NavigationDrawerScaffold extends ConsumerWidget {
             }),
             Padding(padding: const EdgeInsets.all(8.0), child: Divider()),
           ],
-          if (ref.watch(supabaseUserProvider) == null) ...[
-            TextButton.icon(
-              onPressed: () => showDialog(
-                context: context,
-                builder: (context) => AuthDialog(isLogin: true),
-              ),
-              label: Text("Login"),
-              icon: Icon(Icons.person),
-            ),
-            TextButton.icon(
-              onPressed: () => showDialog(
-                context: context,
-                builder: (context) => AuthDialog(isLogin: false),
-              ),
-              label: Text("Register"),
-              icon: Icon(Icons.person_add),
-            ),
-          ] else
-            TextButton.icon(
-              onPressed: () async {
-                await ref.read(supabaseClientProvider).auth.signOut();
-                await ref.read(syncingServiceNotifierProvider).reset();
-                await ref.read(databaseNotifierProvider).clear();
-              },
-              label: Text("Logout"),
-              icon: Icon(Icons.logout),
-            ),
+          AuthButtons(),
         ],
         onDestinationSelected: (index) => context.go(destiantions[index].route),
       ),
