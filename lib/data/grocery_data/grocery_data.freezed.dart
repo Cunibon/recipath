dfaa9da41df599dedc788a6371ebe93f07e7cec8
Cunibon <52969914+Cunibon@users.noreply.github.com>
2025-08-15 11:48:59 +0200

    freezed changes

--- lib/data/grocery_data/grocery_data.freezed.dart
+++ lib/data/grocery_data/grocery_data.freezed.dart
@@ -1,6 +1,5 @@
-// dart format width=80
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -87,6 +86,136 @@ as bool,
 
 }
 
+
+/// Adds pattern-matching-related methods to [GroceryData].
+extension GroceryDataPatterns on GroceryData {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _GroceryData value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _GroceryData() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _GroceryData value)  $default,){
+final _that = this;
+switch (_that) {
+case _GroceryData():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _GroceryData value)?  $default,){
+final _that = this;
+switch (_that) {
+case _GroceryData() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String id,  String name,  double normalAmount,  UnitEnum unit,  double conversionAmount,  UnitEnum conversionUnit,  double? kcal,  double? fat,  double? carbs,  double? protein,  double? fiber,  bool archived,  bool uploaded)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _GroceryData() when $default != null:
+return $default(_that.id,_that.name,_that.normalAmount,_that.unit,_that.conversionAmount,_that.conversionUnit,_that.kcal,_that.fat,_that.carbs,_that.protein,_that.fiber,_that.archived,_that.uploaded);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String id,  String name,  double normalAmount,  UnitEnum unit,  double conversionAmount,  UnitEnum conversionUnit,  double? kcal,  double? fat,  double? carbs,  double? protein,  double? fiber,  bool archived,  bool uploaded)  $default,) {final _that = this;
+switch (_that) {
+case _GroceryData():
+return $default(_that.id,_that.name,_that.normalAmount,_that.unit,_that.conversionAmount,_that.conversionUnit,_that.kcal,_that.fat,_that.carbs,_that.protein,_that.fiber,_that.archived,_that.uploaded);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String id,  String name,  double normalAmount,  UnitEnum unit,  double conversionAmount,  UnitEnum conversionUnit,  double? kcal,  double? fat,  double? carbs,  double? protein,  double? fiber,  bool archived,  bool uploaded)?  $default,) {final _that = this;
+switch (_that) {
+case _GroceryData() when $default != null:
+return $default(_that.id,_that.name,_that.normalAmount,_that.unit,_that.conversionAmount,_that.conversionUnit,_that.kcal,_that.fat,_that.carbs,_that.protein,_that.fiber,_that.archived,_that.uploaded);case _:
+  return null;
+
+}
+}
+
+}
 
 /// @nodoc
 @JsonSerializable()
